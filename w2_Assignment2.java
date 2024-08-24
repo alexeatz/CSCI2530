819965d2755bf038b2b58023552144a2df957291
Nylnarb <57334965+alexeatz@users.noreply.github.com>
2024-08-24 09:46:12 -0400

    Week 2 - Find Common Prefix in 3 String Array

---
+++ w2_Assignment2.java
@@ -0,0 +1,41 @@
+package week1;
+import java.util.Scanner;
+public class w2_Assignment2 {	
+public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("input three words: ");
+	String[] words = new String[3];
+	StringBuilder prefix = new StringBuilder();
+	for (int i = 0; i < 3; i++)
+	{
+		words[i] = sc.next();
+		
+	}
+	int smallest = Integer.MAX_VALUE;
+	for(int i = 0; i < 3; i++)
+	{
+		if (smallest > words[i].length())
+		{
+			smallest = words[i].length();			
+		}
+	}
+	System.out.println("Output :"  ); 
+		
+	for( int j = 0; j < smallest; j++)
+	{
+		char first = words[0].charAt(j);
+		if(first==words[1].charAt(j) && first ==words[2].charAt(j))
+		{
+			
+			prefix.append(first);
+		}
+		else
+		{
+			break;
+		}
+		
+	}
+	System.out.println(prefix.toString());
+
+	}
+}
